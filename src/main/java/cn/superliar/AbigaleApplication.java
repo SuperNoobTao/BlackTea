945513eebb7533f3c888554ad22c7b2458b1069b
沈涛 <387944558@qq.com>
2017-05-08 11:33:40 +0800

    Initial commit

---
+++ src/main/java/cn/superliar/AbigaleApplication.java
@@ -0,0 +1,12 @@
+package cn.superliar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbigaleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AbigaleApplication.class, args);
+	}
+}
